c47a2b7e8f57b6d0ee81fa84672c79d57c637fa0
Loredana <loredana.iosub28@gmail.com>
2018-07-16 10:28:22 +0300

    Initial commit

---
+++ Eshop/Models/Producer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Eshop.Models
+{
+    public class Producer
+    {
+        public int ProducerId { get; set; } //proprietate
+        public string Name { get; set; }
+
+            
+    }
+}
