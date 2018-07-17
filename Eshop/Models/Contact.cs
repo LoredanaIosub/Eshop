b042bbf42a2d36466a76a3d3a041a3abeced2d8a
alban.ionut <ionut.m.alban@gmail.com>
2018-07-17 12:54:04 +0300

    Contact and command model

---
+++ Eshop/Models/Contact.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Eshop.Models
+{
+    public class Contact 
+    {
+        [Key] //Id declarat ca cheie primara
+        public int Id { get; set; }
+
+        [Required]//Necesar
+        [MaxLength(15, ErrorMessage = "FirstName must be 15 characters or less"), MinLength(2)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(15, ErrorMessage = "LastName must be 15 characters or less"), MinLength(2)]
+        public string LastName { get; set; }
+
+        [MaxLength(10, ErrorMessage = "Phone must have 10 numbers"), MinLength(10)]
+        public int Phone { get; set; }
+
+        [Required]
+        [MaxLength(15, ErrorMessage = "Email must be 15 characters or less"), MinLength(2)]
+        public string Email { get; set; }
+
+        [Required] 
+        [MaxLength(300, ErrorMessage = "Message must be minimum 20 characters."), MinLength(20)]
+        public string Message { get; set; }
+    }
+}
