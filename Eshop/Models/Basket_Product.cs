0eb4a55f7c399aaf62e0316f1c0dd0d58e9cb375
Levente Guczui <guczuilevente@outlook.com>
2018-07-17 13:38:39 +0300

    Models.

---
+++ Eshop/Models/Basket_Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Eshop.Models
+{
+    public class Basket_Product
+    {
+        [Key]
+        public int id { get; set; }
+
+        [MaxLength(15)]
+        public string Name { get; set; }
+    }
+}
